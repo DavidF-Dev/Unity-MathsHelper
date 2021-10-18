ba6ed109955cc0b0af68a6b8e89502987de29289
DavidFDev <account@davidfdev.com>
2021-10-19 10:36:46 +1100

    Added empty script files.

---
+++ Runtime/MathsHelper.cs
@@ -0,0 +1,10 @@
+﻿// File: MathsHelper.cs
+// Purpose: Various static maths helper methods
+// Created by: DavidFDev
+
+namespace DavidFDev.Maths
+{
+    public static class MathsHelper
+    {
+    }
+}
---
+++ Runtime/RandomHelper.cs
@@ -0,0 +1,10 @@
+﻿// File: RandomHelper.cs
+// Purpose: Various static helper methods to do with randomness
+// Created by: DavidFDev
+
+namespace DavidFDev.Maths
+{
+    public static class RandomHelper
+    {
+    }
+}
